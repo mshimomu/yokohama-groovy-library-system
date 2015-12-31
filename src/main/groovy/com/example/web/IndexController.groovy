7d5d6f644280ca76c0fed689787063e97337d1f0
mshimomu <masaya.shimomu@gmail.com>
2015-12-31 14:19:20 +0900

    不要なimportを削除

--- src/main/groovy/com/example/web/IndexController.groovy
+++ src/main/groovy/com/example/web/IndexController.groovy
@@ -1,6 +1,5 @@
 package com.example.web
 
-import com.example.domain.User
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
